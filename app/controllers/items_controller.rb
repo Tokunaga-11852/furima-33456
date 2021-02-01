fb79178ec04e98009523e81a11970ca12d1f08b6
Tokunaga-11852 <oneoktoku@gmail.com>
2021-02-01 12:48:12 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!, only: [:new]
+  before_action :definition, only: [:edit, :show, :conditions, :update]
+  before_action :conditions, only: [:edit]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -17,12 +21,39 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
   def show
-    @item = Item.find(params[:id])
   end
 
   private
   def item_params
     params.require(:item).permit(:item_name, :item_information, :category_id, :item_condition_id, :shipping_charge_id, :shipment_source_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
+  
+  def definition
+    @item = Item.find(params[:id])
+  end
+
+  def conditions
+    unless current_user.id == @item.user_id
+    redirect_to action: :index
+    end
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
